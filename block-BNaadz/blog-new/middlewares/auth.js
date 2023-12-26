60db15521416d871278342b19f0d120703d01277
KASHISH <kashu80525@gmail.com>
2023-12-26 11:44:17 +0530

    adding chnges

--- block-BNaadz/blog-new/middlewares/auth.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  loggedInUser: (req, res, next) => {
-    if (req.session && req.session.userId) {
-      next();
-    } else {
-      res.redirect("/users/login");
-    }
-  },
-};
